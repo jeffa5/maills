e96b1bed3efd0677f59b36c3402888830e7b930f
Andrew Jeffery <dev@jeffas.net>
2024-10-20 21:55:37 +0100

    Add tests for mailbox

--- src/mailbox.rs
+++ src/mailbox.rs
@@ -2,7 +2,7 @@ use std::{fmt::Display, str::FromStr};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct Mailbox {
     pub name: Option<String>,
     pub email: String,
@@ -11,7 +11,7 @@ pub struct Mailbox {
 impl Mailbox {
     pub fn from_line_at(line: &str, character: usize) -> Option<Self> {
         let re = regex::Regex::new(
-            r#"(?i)"?(?<name>[\w \-']+)?"? ?<?\b(?<email>[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})\b>?"#,
+            r#"(?i)(?<name>("[\w \-']+"|[\w \-']+))?\s*<?\b(?<email>[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})\b>?"#,
         )
         .unwrap();
         let mut mailbox = None;
@@ -23,7 +23,13 @@ impl Mailbox {
             if let Some(name) = captures.name("name") {
                 start = Some(name.start());
                 end = Some(name.end());
-                mbox.name = Some(name.as_str().trim().to_owned());
+                mbox.name = Some(
+                    name.as_str()
+                        .trim()
+                        .trim_start_matches('"')
+                        .trim_end_matches('"')
+                        .to_owned(),
+                );
             }
             if let Some(email) = captures.name("email") {
                 if start.is_none() {
@@ -33,7 +39,7 @@ impl Mailbox {
                 mbox.email = email.as_str().trim().to_owned();
             }
 
-            if start.map_or(false, |s| s <= character) && end.map_or(false, |e| character < e) {
+            if start.map_or(false, |s| s <= character) && end.map_or(false, |e| character <= e) {
                 mailbox = Some(mbox);
                 break;
             }
@@ -46,9 +52,14 @@ impl FromStr for Mailbox {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Some((name, email)) = s.split_once("\" <") {
-            let name = name.trim_start_matches('"').to_owned();
-            let email = email.trim_end_matches('>').to_owned();
+        if let Some((name, email)) = s.split_once(" <") {
+            let name = name
+                .trim()
+                .trim_start_matches('"')
+                .trim_end_matches('"')
+                .trim()
+                .to_owned();
+            let email = email.trim().trim_end_matches('>').to_owned();
             Ok(Self {
                 name: Some(name),
                 email,
@@ -72,5 +83,78 @@ impl Display for Mailbox {
             write!(f, ">")?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        let s = "First Last <first.last@test.com>";
+        let mbox = Mailbox::from_str(s).unwrap();
+        assert_eq!(
+            mbox,
+            Mailbox {
+                name: Some("First Last".to_owned()),
+                email: "first.last@test.com".to_owned(),
+            }
+        );
+        assert_eq!(Mailbox::from_str(&mbox.to_string()).unwrap(), mbox);
+    }
+
+    #[test]
+    fn from_line_at() {
+        let line = "First Last <first.last@test.com>";
+        let expected = Some(Mailbox {
+            name: Some("First Last".to_owned()),
+            email: "first.last@test.com".to_owned(),
+        });
+        for i in 0..line.len() {
+            assert_eq!(
+                Mailbox::from_line_at(line, i),
+                expected,
+                "character {} {:?}",
+                i,
+                line.chars().nth(i)
+            );
+        }
+    }
+
+    #[test]
+    fn from_line_at_quote() {
+        let line = "\"First Last\" <first.last@test.com>";
+        let expected = Some(Mailbox {
+            name: Some("First Last".to_owned()),
+            email: "first.last@test.com".to_owned(),
+        });
+        for i in 0..line.len() {
+            assert_eq!(
+                Mailbox::from_line_at(line, i),
+                expected,
+                "character {} {:?}",
+                i,
+                line.chars().nth(i)
+            );
+        }
+    }
+
+    #[test]
+    fn from_line_at_context() {
+        let line = "Other words before \"First Last\" <first.last@test.com> and other words after";
+        let expected = Some(Mailbox {
+            name: Some("First Last".to_owned()),
+            email: "first.last@test.com".to_owned(),
+        });
+        for i in 19..53 {
+            assert_eq!(
+                Mailbox::from_line_at(line, i),
+                expected,
+                "character {} {:?}",
+                i,
+                line.chars().nth(i)
+            );
+        }
     }
 }
