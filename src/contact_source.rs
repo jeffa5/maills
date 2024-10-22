b0c7d5fb91e991c282f42c8409554a0a91d1270d
Andrew Jeffery <dev@jeffas.net>
2024-10-22 19:41:23 +0100

    Add meta sources

--- src/contact_source.rs
+++ src/contact_source.rs
@@ -18,3 +18,46 @@ pub trait ContactSource {
     /// Create the contact for the given mailbox, returning the path to it.
     fn create_contact(&mut self, mailbox: Mailbox) -> Option<PathBuf>;
 }
+
+#[derive(Default)]
+pub struct Sources {
+    pub sources: Vec<Box<dyn ContactSource>>,
+}
+
+impl ContactSource for Sources {
+    fn render(&self, mailbox: &Mailbox) -> String {
+        self.sources
+            .iter()
+            .map(|s| s.render(mailbox))
+            .filter(|s| !s.is_empty())
+            .collect::<Vec<_>>()
+            .join("\n\n")
+    }
+
+    fn find_matching(&self, word: &str) -> Vec<Mailbox> {
+        self.sources
+            .iter()
+            .flat_map(|s| s.find_matching(word))
+            .collect()
+    }
+
+    fn contains(&self, mailbox: &Mailbox) -> bool {
+        self.sources.iter().any(|s| s.contains(mailbox))
+    }
+
+    fn filepaths(&self, mailbox: &Mailbox) -> Vec<PathBuf> {
+        self.sources
+            .iter()
+            .flat_map(|s| s.filepaths(mailbox))
+            .collect()
+    }
+
+    fn create_contact(&mut self, mailbox: Mailbox) -> Option<PathBuf> {
+        for s in &mut self.sources {
+            if let Some(path) = s.create_contact(mailbox.clone()) {
+                return Some(path);
+            }
+        }
+        None
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,4 @@ pub use vcards::VCards;
 
 mod contact_source;
 pub use contact_source::ContactSource;
+pub use contact_source::Sources;
